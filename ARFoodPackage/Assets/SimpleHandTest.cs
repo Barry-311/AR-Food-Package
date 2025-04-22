14ee76094d2ae552be9765462887231adb97dda2
theoyjy <ouyangjiayi26@gmail.com>
2025-04-22 19:26:15 +0100

    remove unused scripts and reorganize scripts

--- ARFoodPackage/Assets/SimpleHandTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Mediapipe;  // 基础命名空间
-using Mediapipe.Tasks.Vision.HandLandmarker;
-using Mediapipe.Unity.Sample.HandLandmarkDetection;
-using UnityEngine;
-
-public class HandDemo : MonoBehaviour
-{
-
-}
